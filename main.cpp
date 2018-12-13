64c4006d85cc69d29133f2161cf156dfb4113981
Tibo Clausen <33636216+XenotriX1337@users.noreply.github.com>
2018-12-13 14:21:54 +0100

    Render boids

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <vector>
+
+using BoidList = std::vector<sf::Vector2f>;
+
+const int WIDTH  = 1280;
+const int HEIGHT = 720;
+
+sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Flocking");
+sf::CircleShape shape(10.0f);
+BoidList boids;
+
+void renderBoids();
+void initBoids();
 
 int main() {
   std::cout << "Hello, world!" << std::endl;
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Flocking");
-  sf::CircleShape shape(100.0f);
-  shape.setFillColor(sf::Color::Green);
+
+  initBoids();
 
   while(window.isOpen())
   {
@@ -17,9 +30,28 @@ int main() {
     }
 
     window.clear();
-    window.draw(shape);
+    renderBoids();
     window.display();
   }
 
   return 0;
 }
+
+void initBoids()
+{
+  for(int i = 0; i < 10; i++) {
+    sf::Vector2f pos;
+    pos.x = rand() % WIDTH;
+    pos.y = rand() % HEIGHT;
+    boids.push_back(pos);
+  }
+}
+
+void renderBoids()
+{
+  for(sf::Vector2f pos: boids) {
+    shape.setPosition(pos);
+    window.draw(shape);
+  }
+}
+
