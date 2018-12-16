82632e5b1cbde8a612aa6f51e4b8096c8a9fc012
Tibo Clausen <33636216+XenotriX1337@users.noreply.github.com>
2018-12-16 17:05:55 +0100

    Implement acceleration

--- boid.h
+++ boid.h
@@ -4,5 +4,6 @@ struct Boid
 {
   sf::Vector2f pos;
   sf::Vector2f vel;
+  sf::Vector2f acc;
 };
 
--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ const int WIDTH  = 1280;
 const int HEIGHT = 720;
 const int BOID_COUNT = 100;
 const int PERC_RAD = 100;
+const float MAX_VEL = 0.2f;
 
 sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Flocking");
 sf::CircleShape shape(10.0f);
@@ -21,6 +22,8 @@ BoidList boids;
 void renderBoids();
 void initBoids();
 void wrap();
+void setMagnitude(sf::Vector2f&, float);
+float magnitude(const sf::Vector2f&);
 
 int main() {
   srand(time(NULL));
@@ -36,8 +39,13 @@ int main() {
     }
 
     for(Boid& boid: boids) {
+      boid.vel += boid.acc;
+      if (magnitude(boid.vel) > MAX_VEL)
+        setMagnitude(boid.vel, MAX_VEL);
       boid.pos += boid.vel;
     }
+
+    wrap();
 
     window.clear();
     renderBoids();
@@ -51,8 +59,8 @@ void initBoids()
 {
   for(int i = 0; i < BOID_COUNT; i++) {
     Boid boid;
-    boid.vel.x = (rand() % 4 - 2) / (float)10;
-    boid.vel.y = (rand() % 4 - 2) / (float)10;
+    boid.acc.x = (rand() % 4 - 2) / (float)100;
+    boid.acc.y = (rand() % 4 - 2) / (float)100;
     boid.pos.x = rand() % WIDTH;
     boid.pos.y = rand() % HEIGHT;
     boids.push_back(boid);
@@ -76,5 +84,18 @@ void wrap()
     if (boid.pos.y >= HEIGHT) boid.pos.y -= HEIGHT;
     else if (boid.pos.y < 0) boid.pos.y += HEIGHT;
   }
+}
+
+void setMagnitude(sf::Vector2f& vec, float mag)
+{
+  float currentMag = magnitude(vec);
+  float factor = mag / currentMag;
+  vec.x *= factor;
+  vec.y *= factor;
+}
+
+float magnitude(const sf::Vector2f& vec)
+{
+  return sqrt(pow(vec.x, 2) + pow(vec.y, 2));
 }
 
